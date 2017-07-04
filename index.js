03f05e1867a647ecdbf336773ec8587b39a52e92
Julien Sarazin <julien.sarazin@me.com>
2017-07-04 10:10:25 +0200

    updated sources

--- index.js
+++ index.js
@@ -1,68 +1,23 @@
-const Person = require('./Person');
-let db = [];
+const express = require('express');
+const server = express();
 
-const http = require('http');
-/**
- * Si la requete HTTP est en POST,
- * dans ce cas, ne rien retourner. Attention, il faut quand meme répondre!
- *
- * Si la requette HTTP est en GET,
- * dans ce cas, retourner la liste des utilisateurs créés
- */
+console.log('server listening on port 8080');
 
-const server = http.createServer((request, response) => {
-    const method = request.method;
-    const url = request.url;
-    const path = '/persons';
-
-    if (url.indexOf(path) === -1) {
-        response.statusCode = 404;
-        return response.end(`cannot ${method} ${url}`);
-    }
-
-    // /persons/<le_nom_special>
-    // ['', 'persons', '<le_nom_special>']
-    // 0, 1, 2
-    if (method === 'DELETE') {
-        const components = url.split('/');
-        if (components.length !== 3) {
-            return response.end('invalid path');
-        }
-
-        const name = components[2];
-        db = db.filter(user => user.name !== name);
-        response.end('');
-    }
-
-    if (method === 'POST') {
-        const bucket = [];
-        request.on('data', (chunk) => {
-            bucket.push(chunk);
-        });
-
-        request.on('end', ()=> {
-            const data = bucket.toString();
-            let obj = null;
-            try {
-                obj = JSON.parse(data);
-                if (!obj.name || !obj.age)
-                    throw new Error();
-            }
-            catch (e) {
-                return response.end('tocard');
-            }
-
-            let p = new Person(obj.name, obj.age);
-            db.push(p);
-            return response.end();
-        });
-    }
-
-    else if (method === 'GET') {
-        response.setHeader('content-type', 'application/json');
-        response.end(JSON.stringify(db));
-    }
+server.get('/persons', (req, res, next) => {
+    res.send('list');
 });
 
-console.log('Listening on port ', 8080);
+server.post('/persons', (req, res, next) => {
+    res.send('create');
+});
+
+server.delete('/persons/:name', (req, res, next) => {
+    const name = req.params.name;
+    res.send('delete');
+});
+
+server.put('/persons/:old/:new', (req, res, next) => {
+    res.send('update');
+});
+
 server.listen(8080);
