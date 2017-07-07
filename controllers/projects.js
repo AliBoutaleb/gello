1469f7ed4025d7995319bec4b4f3238a9de91f08
mengyd <yidongmeng1992@gmail.com>
2017-07-07 02:55:10 +0200

    projects.js yidong

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,3 +1,6 @@
+/**
+ * Created by yidon on 06/07/2017.
+ */
 module.exports = (server) => {
     const Project = server.models.Project;
     const User = server.models.User;
@@ -17,11 +20,12 @@ module.exports = (server) => {
 
     function create(req, res) {
         const userId = req.token.userId;
+
         let project = new Project(req.body);
 
         project.creator = userId;
-        createATeam(project);
 
+        createATeam(project);
         return project.save()
             .then(project => res.status(201).send(project))
             .catch(err => res.status(500).send(err));
@@ -29,7 +33,6 @@ module.exports = (server) => {
         function createATeam(project) {
             let team = new Team();
             team.project = project._id;
-            team.members.push(userId);
             project.equipe = team._id;
             return team.save();
         }
@@ -38,12 +41,17 @@ module.exports = (server) => {
     function remove(req, res) {
         findProject(req)
             .then(ensureExist)
+            .then(ensureCreator)
             .then(remove)
             .then(() => res.status(204).send())
             .catch(err => res.status(err.code || 500).send(err.reason || err));
 
         function remove() {
             return Project.findByIdAndRemove(req.params.id);
+        }
+
+        function ensureCreator(project) {
+            return project.creator.toString() === req.token.userId ? project : Promise.reject({code: 403, reason: 'not.allowed'});
         }
     }
 
@@ -58,17 +66,20 @@ module.exports = (server) => {
         function update() {
             return Project.findByIdAndUpdate(req.params.id, req.body)
         }
+        function ensureCreator(project) {
+            return project.creator.toString() === req.token.userId ? project : Promise.reject({code: 403, reason: 'not.allowed'});
+        }
     }
+
+
 
     // Globals
     function findProject(req) {
         return Project.findById(req.params.id)
     }
-    function ensureCreator(project) {
-        return project.creator.toString() === req.token.userId ? project : Promise.reject({code: 403, reason: 'not.allowed'});
-    }
 
     function ensureExist(data) {
         return data ? data : Promise.reject({code: 422, reason: 'unprocessable.entities'});
     }
+
 };
